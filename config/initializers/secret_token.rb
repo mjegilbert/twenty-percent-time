ee666fc41ca7efe3ef304dbbbf7f48a4a72486c7
Mark Gilbert <mgilbert@foundryhiring.com>
2012-01-06 17:41:07 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hcs::Application.config.secret_token = 'f75e190c2b33c470e0ed668ad92b46abc847822e4e31d51750c0420830a6a20520dc6a22dd3bfc9dba36e9ff91918e586548099068f8f04c999b6ff73abefa57'
