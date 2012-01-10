ba1e0b4a663ee43d69e5897cb4443f85d4b41f64
Thaisan Tonthat <ttonthat@gmail.com>
2012-01-10 11:41:11 -0800

    some shit

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -27,9 +27,70 @@ class SiteController < ApplicationController
     @name = "Space Traveler"
   end
   
+  
   def show
     client = LinkedIn::Client.new('4wkfc3lzk92h', '7C2BKYvq04X3K2hB')
     client.authorize_from_access(session[:atoken], session[:asecret])
-    @the_shit = client.connections(:fields =>["id", "first-name", "last-name", "phone-numbers", "main-address","positions", "educations", "honors", "associations", "interests", "picture-url"]) ["all"]
+    @the_shit = client.connections(:fields =>["id", "first-name", "last-name", "phone-numbers", "main-address","positions", "educations", "honors", "associations", "interests", "picture-url"])["all"]
+
+    @the_shit.each do |person|
+      begin
+        # General
+        uuid = person["id"]
+        
+        
+        minion = Minion.new(:first_name => person["first_name"],
+                            :last_name => person["last_name"],
+                            :uuid => uuid)
+        minion.save
+        
+        # # School  
+        # educations = person["educations"]["all"]
+        # if educations
+        #   educations.each do |ed|
+        #     #e = EdSession.new(:start_date => ed["start_date"]["year"], :end_date => ed["end_date"]["year"])
+        #     e = EdSession.new
+        #     e.minion_id = minion.id
+        # 
+        #     existing = School.find_by_name(ed["school_name"])
+        #     if existing
+        #       e.school_id = existing.id
+        #     else
+        #       new_school = School.new(:name => ed["school_name"])
+        #       new_school.save
+        #       e.school_id = new_school.id
+        #     end
+        #       
+        #     e.save
+        #   end
+        # end
+    
+        # Jobs
+        positions = person["positions"]["all"]
+        if positions
+          positions.each do |pos|
+            start_date = DateTime.new(pos["start_date"]["year"].to_i, pos["start_date"]["month"].to_i) rescue DateTime.new(0)
+            end_date = DateTime.new(pos["end_date"]["year"].to_i, pos["end_date"]["month"].to_i) rescue DateTime.new(0)
+                        
+            p = Job.new(:start_date => start_date, :end_date => end_date, :position => pos["title"], :text => pos["summary"])
+            p.minion_id = minion.id
+            
+            # Assign to company
+            existing = Company.find_by_name(pos["company"]["name"])
+            if existing
+              p.company_id = existing.id
+            else
+              new_company = Company.new(:name => pos["company"]["name"])
+              new_company.save
+              p.company_id = new_company.id
+            end        
+
+            p.save                    
+          end
+        end
+      rescue
+        puts "error with " + person.inspect
+      end
+    end
   end
 end
---
+++ lib/tasks/thaisan.rake
@@ -0,0 +1,27 @@
+namespace :thaisan do
+  desc "Dump dat shit"
+  task :dump => :environment do
+    puts "=-=-=-=- Dump database into csv file -=-=-=-="
+
+    file = File.new("dump.csv", "w+")
+    
+    # Write file header
+    file.puts "uuid, first_name, last_name, company, position, start, end"
+    
+    Minion.all.each do |minion|        
+      minion.jobs.each do |j|
+        stuff = [
+                  minion.uuid,
+                  minion.first_name,
+                  minion.last_name,
+                  j.company.name,
+                  j.position,
+                  j.start_date,
+                  j.end_date,
+                ]
+        file.puts stuff.join(", ")
+      end
+    end
+    file.close
+  end
+end
